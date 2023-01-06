b95e5c5342c280a8212ab24f15ecefe96c125bd2
Saahil S Mehta <saahils.mehta@gmail.com>
2023-01-06 23:43:22 +0530

    #2

--- Invert Image/Image_Inversion_CUDA.cpp
+++ Invert Image/Image_Inversion_CUDA.cpp
@@ -5,15 +5,32 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "Inversion_CUDA.h"
 
+#include <algorithm>
+#include <chrono>
+#include <vector>
+using namespace std::chrono;
+
 using namespace std;
 using namespace cv;
+
 
 int main() {
 	Mat Input_Image = imread("Test_Image.png");
 
-	cout << "Height: " << Input_Image.rows << ", Width: " << Input_Image.rows << ", Channels: " << Input_Image.channels() << endl;
+	cout << "Height: " << Input_Image.cols << ", Width: " << Input_Image.rows << ", Channels: " << Input_Image.channels() << endl;
 
-	Image_Inversion_CUDA(Input_Image.data, Input_Image.rows, Input_Image.rows, Input_Image.channels());
+	auto start = high_resolution_clock::now();
+
+	Image_Inversion_CUDA(Input_Image.data, Input_Image.cols, Input_Image.rows, Input_Image.channels());
+
+	auto stop = high_resolution_clock::now();
+	
+	auto duration = duration_cast<microseconds>(stop - start);
+
+	double timee = duration.count();
+
+	cout << "Time taken by function: "
+		<< timee/1000000 << " Seconds" << endl;
 
 	imwrite("Inverted_Image.png", Input_Image);
 	system("pause");
